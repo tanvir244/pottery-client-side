c222db841c2c89b5ee89ebd847c8c06ebc4a14e1
tanvir244 <tr30365s25@gmail.com>
2024-04-29 01:59:48 +0600

    sorting card based on customization value

--- src/pages/MyArtCraftList/MyArtCraftList.jsx
+++ src/pages/MyArtCraftList/MyArtCraftList.jsx
@@ -7,6 +7,7 @@ import MyArtCraftItem from "../MyArtCraftItem/MyArtCraftItem";
 const MyArtCraftList = () => {
     const { user } = useContext(AuthContext);
     const [myItems, setMyItems] = useState([]);
+    const [originalItems, setOriginalItems] = useState([]);
 
     useEffect(() => {
         if (user) {
@@ -14,23 +15,45 @@ const MyArtCraftList = () => {
                 .then(res => res.json())
                 .then(data => {
                     setMyItems(data);
+                    setOriginalItems(data);
                     console.log(data);
                 })
         }
     }, [user]);
 
+    const sortByCustomization = trigger => {
+        if (trigger === "All") {
+            setMyItems(originalItems);
+        } else {
+            const filtered = originalItems.filter(item => item.customization === trigger);
+            setMyItems(filtered);
+        }
+    };
+
     return (
         <div>
             <Navbar></Navbar>
-            <div className="py-12 md:py-16 space-y-4">
-                {
-                    myItems.map(item => <MyArtCraftItem
-                    key={item._id}
-                    item={item}
-                    myItems={myItems}
-                    setMyItems={setMyItems}
-                    ></MyArtCraftItem>)
-                }
+            <div className="py-10">
+                <div className="flex justify-center mb-12">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn m-1 bg-teal-600 text-white font-semibold">Sort by Customization</div>
+                        <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow text-lg font-bold bg-base-100 rounded-box w-52">
+                            <li onClick={() => sortByCustomization('All')}><a>All</a></li>
+                            <li onClick={() => sortByCustomization('Yes')}><a>Yes</a></li>
+                            <li onClick={() => sortByCustomization('No')}><a>No</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="space-y-4">
+                    {
+                        myItems.map(item => <MyArtCraftItem
+                            key={item._id}
+                            item={item}
+                            myItems={myItems}
+                            setMyItems={setMyItems}
+                        ></MyArtCraftItem>)
+                    }
+                </div>
             </div>
             <Footer></Footer>
         </div>
